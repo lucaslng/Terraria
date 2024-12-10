91aa32fa2183d56100bf01ecc6002bf9ab5e9afa
lucaslng <saculgnuel1@gmail.com>
2024-12-10 12:02:00 -0500

    refactor

--- main.py
+++ main.py
@@ -24,6 +24,10 @@ def pixelToCoord(x:int, y:int):
   print(coord)
   return coord
 
+class Entity:
+  def __init__(this, x:float, y:float, width:float, height:float):
+    this.rect = pg.rect.Rect(x, y, width, height)
+
 class Player:
   camera = FRAME.copy()
   camera.center = (BLOCK_SIZE * (WORLD_WIDTH // 2), BLOCK_SIZE * round(WORLD_HEIGHT * 0.55))
@@ -38,13 +42,18 @@ class Player:
   
   
   def move(this):
-    print(this.rect.centerx//20, this.rect.centery//20)
-    this.rect.y += this.gravityvelo
-    this.gravity()
-    if this.hvelo < 0: this.hvelo += min(1, abs(this.hvelo))
+    # print(this.rect.centerx//20, this.rect.centery//20)
+    
+    this.rect.y += this.gravityvelo # gravity drop
+    this.gravity() # update gravityvelo
+    
+    if this.hvelo < 0: this.hvelo += min(1, abs(this.hvelo)) # reduce horizontal velocity constantly to 0
     elif this.hvelo > 0: this.hvelo -= min(1, this.hvelo)
-    this.vvelo += min(0.5, abs(this.vvelo))
+    
+    this.vvelo += min(0.5, abs(this.vvelo)) # reduce vertical velocity constantly to 0
+    
     print(this.hvelo, this.vvelo)
+    
     if world[(this.rect.bottom//20)][((this.rect.centerx+this.hvelo)//20)].name == "Air" and world[(this.rect.top//20)][((this.rect.centerx+this.hvelo)//20)].name == "Air":
       this.rect.x += this.hvelo
     if world[int((this.rect.bottom+this.vvelo)/20)+1][(this.rect.centerx//20)].name == "Air":
@@ -56,15 +65,14 @@ class Player:
   def moveRight(this):
     if this.hvelo < 5: this.hvelo += 2
   def jump(this):
-    if this.onBlock(): this.vvelo -= 7
+    if this.isOnBlock(): this.vvelo -= 7
   def gravity(this):
-    if not this.onBlock():
+    if not this.isOnBlock():
       if this.gravityvelo < 5: this.gravityvelo += GRAVITY
     else:
       this.gravityvelo = 0
     
-    
-  def onBlock(this):
+  def isOnBlock(this):
     if world[(this.rect.bottom//20) + 1][this.rect.centerx//20].name != "Air":
       return True
     else: return False
@@ -83,8 +91,6 @@ class Player:
       SURF.blit(this.texture, FRAME.center)
     else:
       SURF.blit(this.reversedTexture,FRAME.center)
-
-  
   
 player = Player()
 
@@ -100,7 +106,7 @@ class Item:
 
 class Block(Item):
   SIZE = BLOCK_SIZE
-  def __init__(this, name:str, itemTexture, blockTexture, stackSize:int, x:int, y:int):
+  def __init__(this, name:str, itemTexture, blockTexture, stackSize:int, x:int, y:int, isAir=False):
     super().__init__(name, itemTexture, stackSize)
     this.blockTexture = blockTexture
     this.rect = pg.rect.Rect(x*20, y*20, this.SIZE, this.SIZE)
@@ -119,7 +125,7 @@ class Block(Item):
 class Air(Block):
   texture = pg.transform.scale(pg.image.load("air.jpg"), (BLOCK_SIZE, BLOCK_SIZE))
   def __init__(this, x, y):
-    super().__init__("Air", this.texture, this.texture, 0, x, y)
+    super().__init__("Air", this.texture, this.texture, 0, x, y, isAir = True)
 
 class Dirt(Block):
   texture = pg.transform.scale(pg.image.load("dirt.png"), (BLOCK_SIZE, BLOCK_SIZE))
@@ -144,7 +150,9 @@ class World:
   
   def hoveredBlock(this) -> Block:
     mousepos = pg.mouse.get_pos()
-    x, y = pixelToCoord(*mousepos)
+    return this.blockAt(*pixelToCoord(*mousepos))
+  
+  def blockAt(this, x, y) -> Block:
     return this[y][x]
   
   def __getitem__(this, x:int):
