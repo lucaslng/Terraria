c2556ab8e79024992994511d509ea619c74ffb38
lucaslng <saculgnuel1@gmail.com>
2025-01-07 09:12:25 -0500

    make deque in customqueue "private"

--- customqueue.py
+++ customqueue.py
@@ -5,30 +5,30 @@ class Queue:
   '''Queue class that uses collections.deque'''
   
   def __init__(self):
-    self.deque = deque()
+    self._deque = deque()
   
   def pop(self):
     '''Pop the item at the head of the queue'''
-    return self.deque.popleft()
+    return self._deque.popleft()
   
   def add(self, x: Any):
     '''Add an item to the tail of the queue'''
-    self.deque.append(x)
+    self._deque.append(x)
   
   def peek(self):
     '''Peek the item at the head of the queue'''
-    return self.deque[0] if self.deque else None
+    return self._deque[0] if self._deque else None
   
   def tail(self):
     '''Peek the item at the tail of the queue'''
-    return self.deque[-1] if self.deque else None
+    return self._deque[-1] if self._deque else None
   
   def size(self):
     '''Returns the length of the queue'''
-    return len(self.deque)
+    return len(self._deque)
   
   def __len__(self):
-    return len(self.deque)
+    return len(self._deque)
   
   def __repr__(self):
-    return repr(self.deque)
+    return repr(self._deque)
