772f10af556006bb18e5381aebdceaeff2804a31
txxdy <ryanteddy1716@gmail.com>
2025-01-10 13:45:57 -0500

    add perf counter for noise

--- main.py
+++ main.py
@@ -1212,7 +1212,7 @@ class World:
     self.mask = pg.mask.Mask((WORLD_WIDTH*BLOCK_SIZE, WORLD_HEIGHT*BLOCK_SIZE))
     self.lightmap = [
         [0 for x in range(WORLD_WIDTH)] for y in range(WORLD_HEIGHT)]
-
+    
     self.generateWorld()
     self.generateLight()
 
@@ -1225,6 +1225,7 @@ class World:
             'trees': 0.2
         }
     
+    start_time = time.perf_counter()
     # Precompute noise
     grassHeightNoise = SimplexNoise(19, 1)
     stoneHeightNoise = SimplexNoise(30, 1)
@@ -1234,6 +1235,8 @@ class World:
       ore.__name__: (SimplexNoise(ore.veinSize, 2), ore)
       for ore in ores
     }
+    elapsed_time = time.perf_counter() - start_time
+    print(f"Noise time: {elapsed_time:.2f} seconds")
     
     baseProgress = 0.0        
     if self.progress_tracker:
