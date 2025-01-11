efb71507cd656261dd1e2714c8b9ae0a677cd35c
lucaslng <saculgnuel1@gmail.com>
2025-01-11 16:19:16 -0500

    fix

--- game/view/inventory/drawslotcount.py
+++ game/view/inventory/drawslotcount.py
@@ -1,9 +1,9 @@
 import pygame as pg
 from constants import font20
 from game.view import surfaces
-from utils import colors
+from utils import colours
 
 
 def drawSlotCount(count: int, slotCenter: tuple[int, int]) -> None:
-	countText = font20.render(str(count), True, colors.WHITE)
+	countText = font20.render(str(count), True, colours.WHITE)
 	surfaces.hud.blit(countText, slotCenter)
