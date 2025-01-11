3beb08b06ab061627058f4fde66b850a1bc60649
lucaslng <saculgnuel1@gmail.com>
2025-01-11 17:15:22 -0500

    make drawPlayerInventory partial

--- game/view/drawhud/drawplayerinventory.py
+++ game/view/drawhud/drawplayerinventory.py
@@ -1,7 +1,6 @@
+from functools import partial
 from constants import BLOCK_SIZE
-from game.model.items.inventory.inventory import Inventory
 from game.view.inventory.drawinventory import drawInventory
 
 
-def drawPlayerInventory(inventory: Inventory):
-	drawInventory(inventory, 15, 80, BLOCK_SIZE + 2)
+drawPlayerInventory = partial(drawInventory, inventoryx=15, inventoryy=80, slotSize=BLOCK_SIZE+2)
