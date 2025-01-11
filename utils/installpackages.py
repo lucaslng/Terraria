a95604c860b52ba0131cfbb72e71d4e977da8795
lucaslng <saculgnuel1@gmail.com>
2025-01-11 18:14:06 -0500

    uh

--- utils/installpackages.py
+++ utils/installpackages.py
@@ -1,10 +1,20 @@
-import pip
+import subprocess
+import sys
+import importlib.util
 
-_required  = {'numpy','pymunk'}
+_required = {'numpy', 'pymunk'}
+
+def isPackageInstalled(name: str):
+    """Check if a package is installed."""
+    return importlib.util.find_spec(name) is not None
 
 def installPackages():
-	'''Somewhat sketchy way to install packages'''
-	pip.main(['install', *_required])
-
-# def installPackages():
-# 	subprocess.check_call([sys.executable, '-m', 'pip', 'install', *_required])
+    for package in _required:
+        if not isPackageInstalled(package):
+            print(f"{package} not found. Installing...")
+            try:
+                subprocess.check_call([sys.executable, '-m', 'pip', 'install', package])
+            except subprocess.CalledProcessError as e:
+                print(f"Failed to install {package}: {e}")
+        else:
+            print(f"{package} is already installed.")
