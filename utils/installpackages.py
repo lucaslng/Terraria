ae16ee1d8ade8da3373b7c87989e2612d690dca4
lucaslng <saculgnuel1@gmail.com>
2025-01-11 18:13:43 -0500

    uh

--- utils/installpackages.py
+++ utils/installpackages.py
@@ -1,7 +1,10 @@
-import subprocess
-import sys
+import pip
 
 _required  = {'numpy','pymunk'}
 
 def installPackages():
-	subprocess.check_call([sys.executable, '-m', 'pip', 'install', *_required])
+	'''Somewhat sketchy way to install packages'''
+	pip.main(['install', *_required])
+
+# def installPackages():
+# 	subprocess.check_call([sys.executable, '-m', 'pip', 'install', *_required])
