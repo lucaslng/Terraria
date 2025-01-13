62566d29b00fefedbce6fc8b8d0a077c16c3f8e8
lucaslng <saculgnuel1@gmail.com>
2025-01-13 08:24:51 -0500

    cleanup

--- game/view/drawplayer.py
+++ game/view/drawplayer.py
@@ -9,4 +9,4 @@ from game.view import surfaces
 def drawPlayer(player: Player):
 	texture: pg.Surface = sprites["cat"]["walk"][0]
 	texture = pg.transform.scale2x(texture)
-	surfaces.world.blit(texture, (FRAME.centerx - texture.get_width() // 2, FRAME.centery - texture.get_height()//2))
+	surfaces.world.blit(texture, texture.get_rect(center=FRAME.center))
