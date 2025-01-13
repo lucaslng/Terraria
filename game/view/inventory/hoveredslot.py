0c22e7e0516ce74e9e590b90c56579875fbf3b0e
lucaslng <saculgnuel1@gmail.com>
2025-01-12 21:10:21 -0500

    cleanup

--- game/view/inventory/hoveredslot.py
+++ game/view/inventory/hoveredslot.py
@@ -3,8 +3,6 @@ from game.model.items.inventory.inventory import Inventory
 
 from pygame import mouse
 
-from game.model.items.inventory.slot import Slot
-
 def getHoveredSlotRect(*inventories: tuple[Inventory, int, int, int]) -> Rect | None:
 	'''check whether the mouse is hovering over a slot and return the rect of the slot to be drawn. Returns none if the cursor is not hovering any slot. inventories is given in a tuple (inventory, slotSize, x, y)'''
 	
@@ -12,8 +10,8 @@ def getHoveredSlotRect(*inventories: tuple[Inventory, int, int, int]) -> Rect | 
 
 	for inventory, slotSize, inventoryx, inventoryy in inventories:
 		slotRect = Rect(0, 0, slotSize, slotSize)
-		for r, row in enumerate(inventory.array):
-			for c, slot in enumerate(row):
+		for r in range(inventory.rows):
+			for c in range(inventory.cols):
 				x = inventoryx + c * slotSize
 				y = inventoryy + r * slotSize
 				rect = slotRect.copy()
@@ -30,8 +28,8 @@ def getHoveredSlotSlot(inventories: dict[str, tuple[Inventory, int, int, int]]) 
 	for name, inventorydata in inventories.items():
 		inventory, slotSize, inventoryx, inventoryy = inventorydata
 		slotRect = Rect(0, 0, slotSize, slotSize)
-		for r, row in enumerate(inventory.array):
-			for c, slot in enumerate(row):
+		for r in range(inventory.rows):
+			for c in range(inventory.cols):
 				x = inventoryx + c * slotSize
 				y = inventoryy + r * slotSize
 				rect = slotRect.copy()
