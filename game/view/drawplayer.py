fa144cb6712e63e5678e079a9e6058df7b08bc71
lucaslng <saculgnuel1@gmail.com>
2025-01-13 08:57:46 -0500

    animations

--- game/view/drawplayer.py
+++ game/view/drawplayer.py
@@ -6,26 +6,18 @@ from game.view import surfaces
 
 
 def drawPlayer(player: Player):
-	# texture: pg.Surface = sprites["cat"]["walk"][0]
+	'''Draw player with animations'''
+	
+	# print(round(player.velocity.x, 2), round(player.velocity.y, 2))
 	textures: dict[str, Animation] = sprites["cat"]
-	# texture = pg.transform.scale2x(texture)
-	# surfaces.world.blit(texture, texture.get_rect(center=FRAME.center))
 
-	if -0.1 < player.velocity.x < 0.1:
+	if -0.05 < player.velocity.x < 0.05:
 		textures["sit"].drawAnimated(surfaces.world, FRAME.center)
 	else:
 		flipped: bool = player.velocity.x < 0
-		
-		if player.velocity.y < -0.9:
+		if player.velocity.y < -0.5:
 			textures["jump"].drawFrame(surfaces.world, FRAME.center, 2, flipped)
-		elif player.velocity.y > 0.9:
+		elif player.velocity.y > 0.5:
 			textures["jump"].drawFrame(surfaces.world, FRAME.center, 4, flipped)
 		else:
 			textures["walk"].drawAnimated(surfaces.world, FRAME.center, flipped)
-	
-
-	# flipped = player.velocity.x < 0
-
-	# if player.velocity.y < -4:
-	# 	sprites["cat"]["jump"].drawFrame(FRAME.center, 2, flipped)
-	# el
