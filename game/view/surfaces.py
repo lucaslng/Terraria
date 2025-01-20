54da1670d9a722543a1cc3eb82a584ac88dfbeb5
lucaslng <saculgnuel1@gmail.com>
2025-01-20 12:14:56 -0500

    decrease hud size

--- game/view/surfaces.py
+++ game/view/surfaces.py
@@ -5,7 +5,7 @@ from utils.constants import BLOCK_SIZE, HEIGHT, WIDTH
 world = Surface((WIDTH, HEIGHT), SRCALPHA)
 blockBreak = world.copy()
 sunlight = world.copy()
-hud = world.copy()
+hud = Surface((WIDTH, int(HEIGHT * 0.6)), SRCALPHA)
 hotbar = Surface((int(BLOCK_SIZE * 13.5), int(BLOCK_SIZE * 1.5)))
 hotbar.set_colorkey(colours.WEIRD)
 health = Surface((BLOCK_SIZE * 18, BLOCK_SIZE))
