51bbc3d364407723601f6a248a7d6e057f4274a7
lucaslng <saculgnuel1@gmail.com>
2025-01-21 08:56:58 -0500

    make draw liquids more efficient

--- game/view/drawliquids.py
+++ game/view/drawliquids.py
@@ -3,12 +3,14 @@ import pygame as pg
 
 from game.view import conversions, surfaces
 from utils import colours
-from utils.constants import BLOCK_SIZE
+from utils.constants import BLOCK_SIZE, FRAME
 
 
 def drawLiquids(liquids: list[Liquid], camera: pg.Rect):
 	'''draw the liquids in the world'''
-
+	
 	for liquid in liquids:
 		for liquidParticle in liquid.particles:
-			pg.draw.circle(surfaces.world, colours.BLUE, conversions.coordinate2Pixel(*liquidParticle.body.position, camera), liquidParticle.shape.radius * BLOCK_SIZE * 5)
+			pos = conversions.coordinate2Pixel(*liquidParticle.body.position, camera)
+			if FRAME.collidepoint(*pos):
+				pg.draw.circle(surfaces.world, colours.BLUE, pos, liquidParticle.shape.radius * BLOCK_SIZE * 5)
--- utils/constants.py
+++ utils/constants.py
@@ -17,7 +17,7 @@ font16 = pg.font.Font("assets/MinecraftRegular-Bmg3.otf", 16)
 BLOCK_SIZE = 32
 BLOCK_RECT = pg.rect.Rect(0, 0, BLOCK_SIZE, BLOCK_SIZE)
 WORLD_HEIGHT = 100
-WORLD_WIDTH = 200    #default is 1000
+WORLD_WIDTH = 400    #default is 1000
 
 BIG = 2147483647
 
