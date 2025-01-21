86dce15d2fc423c97b95ae0cf4f59e944e099752
txxdy <ryanteddy1716@gmail.com>
2025-01-21 14:13:04 -0500

    some furnace things

--- game/model/blocks/furnaceblock.py
+++ game/model/blocks/furnaceblock.py
@@ -5,6 +5,7 @@ from game.model.blocks.utils.inventoryblock import InventoryBlock
 from game.model.items.item import Item
 from game.model.items.inventory.inventory import Inventory
 from game.model.items.inventory.inventorytype import InventoryType
+from game.model.items.specialitems.fuel import Fuel
 from game.model.items.specialitems.smeltable import Smeltable
 from game.model.items.utils.itemsenum import Items
 from game.model.items.inventory.slot import Slot
@@ -13,13 +14,9 @@ from game.model.items.inventory.slot import Slot
 class FurnaceBlock(Block, InventoryBlock):
     '''Furnace block that can smelt items using fuel'''
     def __init__(self):
-        self.inputSlot = Inventory(1, 1)
-        self.fuelSlot = Inventory(1, 1, condition=lambda other: isinstance(other.item, Smeltable) or other.item is None)
-        self.outputSlot = Inventory(1, 1, condition=lambda other: other.item is None)
-        
-        self.currentBurnTime = 0            # How much burn time is left
-        self.currentSmeltTime = 0           # Progress of current smelting operation
-        self.maxCurrentBurnTime = 0         # Total burn time of current fuel
+        self.inputSlot = Inventory(1, 1, condition=lambda other: isinstance(other.item, Smeltable) or other.item is None)           #only smeltable items
+        self.fuelSlot = Inventory(1, 1, condition=lambda other: isinstance(other.item, Fuel) or other.item is None)                 #only fuel items
+        self.outputSlot = Inventory(1, 1, condition=lambda other: other.item is None)                                               #can't put items in
         
         super().__init__(0.94, 3.5, BlockType.PICKAXE, Blocks.Furnace)
         
@@ -31,3 +28,5 @@ class FurnaceBlock(Block, InventoryBlock):
             (self.fuelSlot, InventoryType.FuelIn),
             (self.outputSlot, InventoryType.FurnaceOut)
         )
+        
+    
--- game/model/items/specialitems/smeltable.py
+++ game/model/items/specialitems/smeltable.py
@@ -1,12 +1,6 @@
 from dataclasses import dataclass
 
-
 @dataclass
 class Smeltable:
-    '''Items that can be used as fuel in furnaces'''
-
-    burnTime: int              # How many game ticks this fuel burns for
-    heatOutput: float          # Multiplier for burn effectiveness (1.0 is standard)
-    
-    def __post_init__(self):
-        self.initialBurnTime = self.burnTime
+    '''Items that are smeltable in a furnace'''
+    pass
