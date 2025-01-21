98adc09886ef2612e10fb1d5494a17da49312bf7
txxdy <ryanteddy1716@gmail.com>
2025-01-20 21:24:56 -0500

    fully working pause menu

--- game/game.py
+++ game/game.py
@@ -103,6 +103,13 @@ def game() -> Screens:
 
 	while True:
 		clearScreen()
+		draw(model, camera, inventories)
+  
+		#player death
+		if not model.update():
+			saving.clear()
+			return deathScreen(pg.image.tobytes(SURF, 'RGBA'))
+  
 		pressedKeys = pg.key.get_pressed()
 		if pressedKeys[userKeys.walkLeft]:
 			model.player.walkLeft()
@@ -159,7 +166,11 @@ def game() -> Screens:
 				pg.mixer.set_num_channels(len(model.entities) * 2) # set extra channels just to be safe
 			elif event.type == pg.KEYDOWN:
 				if event.key == pg.K_ESCAPE:
-					return pauseMenu(model, pg.image.tobytes(SURF, 'RGBA'))
+					pause = pauseMenu(model, pg.image.tobytes(SURF, 'RGBA'))   
+					if pause == None:
+						break
+					else:
+						return pause
 				elif event.key == userKeys.interact:
 					if len(inventories) > 1:
 						inventories = {InventoryType.Player: inventories[InventoryType.Player]}
@@ -226,11 +237,5 @@ def game() -> Screens:
 					model.world[y][x].update()
 
 		camera.center = model.player.position[0] * BLOCK_SIZE, model.player.position[1] * BLOCK_SIZE		
-		draw(model, camera, inventories)
-  
-		#player death
-		if not model.update():
-			saving.clear()
-			return deathScreen(pg.image.tobytes(SURF, 'RGBA'))
 
 		updateScreen()
